305e19c09523313bfc45182824aeef9cf073154c
Cristhian Caicedo <cristh2592@gmail.com>
2016-07-04 22:06:02 -0430

    Integrado del Menu Principal

---
+++ Spaceman/Assets/Scripts_spaceman/db_spaceman.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+public class db_spaceman : MonoBehaviour {
+
+    
+    public bool pooling = true;
+    private string cadenaConeccion, comando, user, pass;
+    private MySqlConnection conexMysql = null;
+    private MySqlCommand cmdMysql = null;
+    private MySqlDataReader rdrMysql = null;
+
+    public void conectar() {
+
+        cadenaConeccion = "Server=127.0.0.1" + ";Database=spaceman" + ";User=root" + ";Password=";
+        /*+ ";Pooling=";
+        if (pooling) {
+            cadenaConeccion += "true;";
+        }
+        else {
+            cadenaConeccion += "false;";
+        }*/
+
+        try {
+            conexMysql = new MySqlConnection(cadenaConeccion);
+            conexMysql.Open();
+            Debug.Log("Estado de MySql: " + conexMysql.State);
+        } catch (Exception e) {
+            Debug.Log(e);
+        }
+    }
+
+    void OnApplicationQuit() {
+        if (conexMysql != null) {
+            if (conexMysql.State.ToString() != "Closed") {
+                conexMysql.Close();
+                Debug.Log("Conexion a MySql cerrada");
+            }
+            conexMysql.Dispose();
+        }
+    }
+
+    public string GetEstadoConexion() {
+        return this.conexMysql.State.ToString();
+    }
+
+    public bool consultar(string username, string password)
+    {
+        try
+        {
+            comando = "SELECT * FROM login_user";
+            MySqlCommand cmdMysql = new MySqlCommand(comando, conexMysql);
+            rdrMysql = cmdMysql.ExecuteReader();
+            while (rdrMysql.Read())
+            {
+                user = rdrMysql.GetString(0);
+                pass = rdrMysql.GetString(1);
+            }
+        }
+        catch (MySqlException err)
+        {
+            Console.WriteLine("Error: " + err.ToString());
+        }
+
+        if (username.Equals(user) && password.Equals(pass))
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+        
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Spaceman/Assets/Scripts_spaceman/inicio_sesion.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class inicio_sesion : MonoBehaviour {
+
+    public Button btn_jugar;
+    public Text p_conexion, p_conexion2;
+    public InputField inp1, inp2;
+    public db_spaceman bdd;
+    public bool comprobar;
+
+    
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void Awake()
+    {
+        btn_jugar = GetComponent<Button>();
+        btn_jugar.onClick.AddListener(() => { cambiarTexto(); });
+    }
+
+    void cambiarTexto()
+    {
+        bdd = new db_spaceman();
+        p_conexion = GameObject.Find("pruebaconex").GetComponent<Text>();
+        p_conexion2 = GameObject.Find("pruebaconex2").GetComponent<Text>();
+        inp1 = GameObject.Find("userInput").GetComponent<InputField>();
+        inp2 = GameObject.Find("passwInput").GetComponent<InputField>();
+        bdd.conectar();
+        p_conexion.text = bdd.GetEstadoConexion();
+        comprobar = bdd.consultar(inp1.text, inp2.text);
+        if (comprobar == true)
+        {
+            p_conexion2.text = "Usuario conectado!";
+        } else {
+            p_conexion2.text = "Usuario incorrecto...";
+        }
+    }
+    
+}
