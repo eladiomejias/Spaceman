d3940de6f33745eb7a7e3e1e27aec0aa044e736a
Eladio <eladioj6@hotmail.com>
2016-07-09 14:17:57 -0400

    vida enemigo / status bar

--- Spaceman/Assets/Enemy.cs
+++ Spaceman/Assets/Enemy.cs
@@ -7,21 +7,65 @@ public class Enemy : MonoBehaviour {
 	[System.Serializable]
 	public class EnemyStats{
 		/* Constructor */
-		public float Vida = 100f;
+
+		public int maximaVida = 100;
+		private int _vidaActual;
+
+		public int vidaActual{
+
+
+
+			get{ return _vidaActual; }
+			set{ _vidaActual = Mathf.Clamp (value, 0, maximaVida);}
+
+
+		}
+
+		public void Init(){
+
+			vidaActual = maximaVida;
+
+		}
+
 
 	}
 
-	/* Creand instancia de la clase player */
+
+
+
+	/* Creando instancia de la clase player */
 	public EnemyStats myenemy = new EnemyStats();
+
+	[Header ("Optional: ")]
+	[SerializeField]
+	private StatusIndicator statusIndicator;
+	void Start(){
+
+		myenemy.Init ();
+
+		if(statusIndicator != null){
+
+			statusIndicator.SetHealth (myenemy.vidaActual, myenemy.maximaVida);
+
+		}
+
+	}
+
 
 
 	/* Metodo del daño */
 	public void DamageEnemy(int damage){
-		myenemy.Vida -= damage;
+		myenemy.vidaActual -= damage;
 
-		if(myenemy.Vida <= 0){
+		if(myenemy.vidaActual <= 0){
 			//Debug.LogError ("Myplayer die");
 			GameMaster.KillEnemy(this);
+		}
+
+		if(statusIndicator != null){
+
+			statusIndicator.SetHealth (myenemy.vidaActual, myenemy.maximaVida);
+
 		}
 	}
 
---
+++ Spaceman/Assets/StatusIndicator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour {
+
+	[SerializeField]
+	private RectTransform statusVidaRect;
+	[SerializeField]
+	private Text statusVidaText;
+
+	void Start(){
+		// Verificacion si existen elementos llamados en la scene.
+		if(statusVidaRect == null){
+			Debug.LogError ("STATUS: No se encontro ningun elemento de status vida.");
+		}
+
+		if(statusVidaText == null){
+			Debug.LogError ("STATUS: No se encontro ningun elemento de status vida texto.");
+		}
+
+	}
+
+	// Accesible solo en el metodo.
+	public void SetHealth(int _cur, int _max){
+
+		float _value = (float)_cur / _max;
+
+		statusVidaRect.localScale = new Vector3 (_value, statusVidaRect.localScale.y, statusVidaRect.localScale.z);
+		statusVidaText.text = _cur + "/" + _max + "HP";
+	}
+}
